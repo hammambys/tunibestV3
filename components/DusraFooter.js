062cc0a4556c21ae7e33a197d5e8fd7e809e6686
hammam <hammambenyounes4@gmail.com>
2022-09-07 10:54:11 +0100

    first

---
+++ components/DusraFooter.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const DusraFooter = () => {
+  return (
+    <div>
+      <div className=" bg-[#1b1b1b] h-20 mt-10 items-center flex justify-center text-white">
+        <p>footer</p>
+      </div>
+    </div>
+  );
+};
+
+export default DusraFooter;
